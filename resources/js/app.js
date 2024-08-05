3e6f476ebfcde919a1e1280ae706051af01bd338
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-08-05 15:54:46 +0800

    11 Global Component Registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Link } from '@inertiajs/vue3';
 
 createInertiaApp({
 	resolve: (name) => {
@@ -7,7 +7,7 @@ createInertiaApp({
 		return pages[`./Pages/${name}.vue`];
 	},
 	setup({ el, App, props, plugin }) {
-		createApp({ render: () => h(App, props) }).use(plugin).mount(el);
+		createApp({ render: () => h(App, props) }).use(plugin).component('Link', Link).mount(el);
 	},
 	progress: {
 		color: 'red',
