7ba2e7ce5d923153662ec573742c7e9e2625971c
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-08-05 11:34:10 +0800

    02 | 03 - Install and Configure Inertia | Pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Home', [
+        'name' => 'Jeffrey Way',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
+    ]);
 });
