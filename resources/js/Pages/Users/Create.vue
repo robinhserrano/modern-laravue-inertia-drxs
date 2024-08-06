48c89a469b5eb7fd8441ac9c9b58670f74d6ef2b
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-08-06 13:05:49 +0800

    20 Display Failed Validation Messages

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -9,6 +9,7 @@
       <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name"> Name </label>
 
       <input v-model="form.name" class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required />
+      <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
     </div>
 
     <div class="mb-6">
@@ -16,6 +17,8 @@
 
       <input v-model="form.email" class="border border-gray-400 p-2 w-full" type="email" name="email" id="email"
         required />
+
+      <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
     </div>
 
     <div class="mb-6">
@@ -23,6 +26,8 @@
 
       <input v-model="form.password" class="border border-gray-400 p-2 w-full" type="password" name="password"
         id="password" required />
+
+      <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
     </div>
 
     <div class="mb-6">
@@ -34,6 +39,10 @@
 <script setup>
 import { reactive } from 'vue';
 import { router } from '@inertiajs/vue3';
+
+defineProps({
+  errors: Object
+});
 
 let form = reactive({
   name: '',
