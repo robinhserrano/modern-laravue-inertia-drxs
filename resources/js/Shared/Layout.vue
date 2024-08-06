5d5cb82856f985886b3d8ba51746a76290c0b5a5
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-08-06 09:11:07 +0800

    15 Dynamic Title and Meta Tags

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,4 +1,9 @@
 <template>
+
+    <Head>
+        <meta type="description" content="Information about my app" head-key="description">
+    </Head>
+
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
             <div class="flex items-center">
@@ -19,7 +24,7 @@
         <div class="max-w-3xl mx-auto">
             <iframe class="mb-6" width="100%" frameborder="no" scrolling="no" seamless
                 src="https://player.simplecast.com/fd0bd2ba-c553-466c-a060-b144797ce369?dark=false" />
-                
+
             <slot />
         </div>
     </section>
